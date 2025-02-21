90c35c3783fda3e63e29c45f5cf419871a901d95
Raeann <raeann.tsui@gmail.com>
2025-02-21 11:39:59 -0800

    fixed routing issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import Image from "next/image";
-import NavigationBar from "@/components/landingpage";
-// import { HeaderMegaMenu } from "@/components/landingpage";
-import Head from "next/head";
 export default function Home() {
   return (
     <>
-      <NavigationBar />
-      <div>This is the homepage content!</div>
+      <div>Welcome to Q Nine and Hair Salon!</div>
     </>
   );
 }
---
+++ src/components/header/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+export default function Header() {
+  return (
+    <>
+      <Link href="/">Home</Link>
+      <Link href="/about">About Us</Link>
+      <Link href="/services">Services</Link>
+      <Link href="/booking">Booking</Link>
+      <Link href="/gallery">Gallery</Link>
+      <Link href="/contactus">Contact Us</Link>
+    </>
+  );
+}
