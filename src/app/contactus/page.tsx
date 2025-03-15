a007712cfd8d4ead94d38a797d4920236f98a492
Raeann <raeann.tsui@gmail.com>
2025-03-15 02:00:33 -0700

    barebones design for contact us page

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -17,13 +17,20 @@ export default function ContactUs() {
       info: "669-649-9639",
     },
   ];
+
+  const messageBox = [
+    { title: "Your Name" },
+    { title: "Your Email" },
+    { title: "Your Subject" },
+    { title: "Your Message" },
+  ];
   return (
     <>
       <div className="flex items-center my-4">
         <h2 className="text-center py-2 font-bold text-2xl">CONTACT US</h2>
         <hr className="flex-grow border-t-2 border-gray-300 mx-4" />
       </div>
-      <div className="flex flex-row border-2 border-solid border-red-600">
+      <div className="flex flex-row ">
         {contactInfo.map((card, idx) => (
           <div key={idx} className="flex flex-row">
             <div className="">
@@ -41,6 +48,26 @@ export default function ContactUs() {
             </div>
           </div>
         ))}
+      </div>
+      <div className="flex flex-row items-center justify-around">
+        <div>
+          <h1>Send Message</h1>
+          {messageBox.map((field, idx) =>
+            field.title === "Your Message" ? (
+              <div key={idx}>
+                <p>{field.title}</p>
+                <textarea placeholder={field.title}></textarea>
+              </div>
+            ) : (
+              <div key={idx}>
+                <p>{field.title}</p>
+                <input placeholder={field.title} type="text" />
+              </div>
+            )
+          )}
+          <button>Send Message</button>
+        </div>
+        <div>Placeholder for Google Maps</div>
       </div>
     </>
   );
